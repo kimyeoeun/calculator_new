05f5380b56b91910524890e1848cf832ce245a68
developer1 <kdudms@naver.com>
2023-04-03 14:59:31 +0900

    Modify ctrl.py to connect btnl to calculate method

--- ui.py
+++ ui.py
@@ -1,40 +1,11 @@
-from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-                             QMessageBox, QPlainTextEdit, QHBoxLayout)
-from PyQt5.QtGui import QIcon
-
-
-class View(QWidget):
-    
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+class Control:
+    def __init__(self, view):
+        self.view = view
+        self.connectSignals()
         
-    def initUI(self):
-        self.te1 = QPlainTextEdit()
-        self.te1.setReadOnly(True)
+    def calculate(self):
+        pass
         
-        self.btn1=QPushButton('Message',self)
-        self.btn2=QPushButton('Clear',self)
-        
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(self.btn1)
-        hbox.addWidget(self.btn2)
-        
-        vbox=QVBoxLayout()
-        vbox.addWidget(self.te1)
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)
-        
-        self.setLayout(vbox)
-        
-        self.setWindowTitle('Calculator')
-        self.setWindowIcon(QIcon('icon.png'))
-        self.resize(256,256)
-        self.show()
-        
-    def activateMessage(self):
-        self.te1.appendPlainText("Button clicked!")
-        
-    def clearMessage(self):
-        self.te1.clear()
+    def connectSignals(self):
+        self.view.btn1.clicked.connect(self.calculate)
+        self.view.btn2.clicked.connect(self.view.clearMessage)
